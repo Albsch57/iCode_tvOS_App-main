eb7280ee1916935f97f9468545660de971a4187a
Alona <albsch57@gmail.com>
2022-06-27 18:47:23 +0300

    Initial Commit

---
+++ tvOS_First_Test/Search/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  tvOS_First_Test
+//
+//  Created by Alona on 26/06/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    var searchController: UISearchController!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
